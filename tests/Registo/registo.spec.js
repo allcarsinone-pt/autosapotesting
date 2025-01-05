fe01a4f46a9823dba1712d527c928d06f031324d
goncalos10 <a18847@alunos.ipca.pt>
2025-01-05 16:56:11 +0000

    Added expect, added test config to test-config.json

--- tests/Registo/registo.spec.js
+++ tests/Registo/registo.spec.js
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { generate } from "generate-password";
+import { describe } from 'node:test';
+const config = require("../../test-config.json").registarUtilizador;
 let context;
 let page;
 
@@ -11,73 +13,133 @@ var passwordValue = generate({
   lowercase: true
 })
 
-test('should register an user', async ({ browser }) => {
+describe('User register', () => {
+  test.beforeAll(async ({ browser }) => {
+    context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto(config.global);
+    await page.waitForTimeout(5000);
 
-  context = await browser.newContext();
-  page = await context.newPage();
-  await page.goto("https://auto.sapo.pt/");
-  await page.waitForTimeout(10000);
+    const cookieScreen = page.locator('//*[@id="qc-cmp2-ui"]');
+    if (await cookieScreen.isVisible()) {
+      const acceptCookies = page.locator('//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]');
+      await acceptCookies.click();
+    }
 
-  const cookieScreen = page.locator('//*[@id="qc-cmp2-ui"]');
-  if (await cookieScreen.isVisible()) {
-    const acceptCookies = page.locator('//*[@id="qc-cmp2-ui"]/div[2]/div/button[3]');
-    await acceptCookies.click();
-  }
+  })
 
-  const buttonRegister = page.locator('//*[@id="navbarCollapse"]/ul/li[6]/button');
-  if (await buttonRegister.isVisible()) {
-    await buttonRegister.click();
-  }
 
-  const emailInput = page.locator('//*[@id="username"]');
-  const emailRegister = "grandiosoteixas2002@gmail.com";
-  await emailInput.fill(emailRegister);
+  test('should register an user', async ({ context }) => {
 
-  let buttonContinue = page.locator('//*[@id="submit-username"]');
-  await buttonContinue.click();
+    await page.goto(config.global);
 
-  if (await page.locator('//*[@id="/html/body/div[1]/div[1]/div/div/div/form/div"]').isVisible()) {
-    await page.pause();
-  }
+    const buttonRegister = page.locator('//*[@id="navbarCollapse"]/ul/li[6]/button');
+    if (await buttonRegister.isVisible()) {
+      await buttonRegister.click();
+    }
 
-  const gender = page.locator('//*[@id="registerGender"]/div[2]');
-  const acceptTerms = page.locator('//*[@id="terms"]');
+    const emailInput = page.locator('//*[@id="username"]');
+    await emailInput.fill(config.tests[0].email);
 
-  // Verificar abordagem
-  if (!(await gender.isVisible())) {
-    console.log("Email already registered");
+    let buttonContinue = page.locator('//*[@id="submit-username"]');
+    await buttonContinue.click();
+
+    if (await page.locator('//*[@id="/html/body/div[1]/div[1]/div/div/div/form/div"]').isVisible()) {
+      await page.pause();
+    }
+
+    const gender = page.locator('//*[@id="registerGender"]/div[2]');
+    const acceptTerms = page.locator('//*[@id="terms"]');
+
+    // Verificar abordagem
+    if (!(await gender.isVisible())) {
+      console.log("Email already registered");
+      await context.close();
+    }
+
+    await page.pause(); // Pausa para colocar código de validação recebido no email
+    await gender.click();
+    await acceptTerms.click();
+
+    buttonContinue = page.locator('//*[@id="btn-token-submit"]');
+    await page.waitForTimeout(3000);
+    await buttonContinue.click();
+
+    const acceptCreatePassword = page.locator('//*[@id="content"]/div[3]/div[2]/a');
+    await acceptCreatePassword.click();
+
+    const password = page.locator('//*[@id="current-password"]');
+    const confirmPassword = page.locator('//*[@id="new-password"]');
+
+    await password.fill(passwordValue);
+    await confirmPassword.fill(passwordValue);
+
+    console.log(passwordValue);
+
+    buttonContinue = page.locator('//*[@id="btn-add-password"]');
+    await buttonContinue.click();
+
+    await page.goto(config.global);
+
+    const isLoggedIn = page.locator('//*[@id="navbarCollapse"]/ul/li[6]/div/span');
+
+    const emailText = await isLoggedIn.textContent();
+    expect(emailText).toContain('Olá');
+
     await context.close();
-  }
 
-  await page.pause();
-  await gender.click();
-  await acceptTerms.click();
+  });
 
-  buttonContinue = page.locator('//*[@id="btn-token-submit"]');
-  await page.waitForTimeout(3000);
-  await buttonContinue.click();
+  test('should not register an user because passwords are different', async ({ context }) => {
 
-  const acceptCreatePassword = page.locator('//*[@id="content"]/div[3]/div[2]/a');
-  await acceptCreatePassword.click();
+    await page.goto(config.global);
 
-  const password = page.locator('//*[@id="current-password"]');
-  const confirmPassword = page.locator('//*[@id="new-password"]');
+    const buttonRegister = page.locator('//*[@id="navbarCollapse"]/ul/li[6]/button');
+    if (await buttonRegister.isVisible()) {
+      await buttonRegister.click();
+    }
 
-  await password.fill(passwordValue);
-  await confirmPassword.fill(passwordValue);
+    const emailInput = page.locator('//*[@id="username"]');
+    await emailInput.fill(config.tests[0].email);
 
-  console.log(passwordValue);
+    let buttonContinue = page.locator('//*[@id="submit-username"]');
+    await buttonContinue.click();
 
-  buttonContinue = page.locator('//*[@id="btn-add-password"]');
-  await buttonContinue.click();
+    if (await page.locator('//*[@id="/html/body/div[1]/div[1]/div/div/div/form/div"]').isVisible()) {
+      await page.pause();
+    }
 
-  await page.goto("https://auto.sapo.pt/");
+    const gender = page.locator('//*[@id="registerGender"]/div[2]');
+    const acceptTerms = page.locator('//*[@id="terms"]');
 
-  const isLoggedIn = page.locator('//*[@id="navbarCollapse"]/ul/li[6]/div/span');
+    // Verificar abordagem
+    if (!(await gender.isVisible())) {
+      console.log("Email already registered");
+      await context.close();
+    }
 
-  const emailText = await isLoggedIn.textContent();
-  expect(emailText).toContain('Olá');
+    await page.pause(); // Pausa para colocar código de validação recebido no email
+    await gender.click();
+    await acceptTerms.click();
 
-  await context.close();
+    buttonContinue = page.locator('//*[@id="btn-token-submit"]');
+    await page.waitForTimeout(3000);
+    await buttonContinue.click();
 
-});
+    const acceptCreatePassword = page.locator('//*[@id="content"]/div[3]/div[2]/a');
+    await acceptCreatePassword.click();
+
+    const password = page.locator('//*[@id="current-password"]');
+    const confirmPassword = page.locator('//*[@id="new-password"]');
+
+    await password.fill(passwordValue);
+    await confirmPassword.fill(passwordValue + '1');
+
+    buttonContinue = page.locator('//*[@id="btn-add-password"]');
+
+
+    await context.close();
+
+  });
+
+})
