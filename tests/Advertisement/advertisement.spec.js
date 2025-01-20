f51ed0608e3dff7c6353d9d30d14e823f1853087
goncalos10 <a18847@alunos.ipca.pt>
2025-01-20 15:44:13 +0000

    Created test to insert an advertisement

--- tests/Advertisement/advertisement.spec.js
+++ tests/Advertisement/advertisement.spec.js
@@ -12,129 +12,106 @@ export default function createAdvertisement() {
             await page.goto(config.global);
 
 
-            const emailInput = page.locator('//*[@id="username"]');
-            await emailInput.fill(config.tests[0].email);
+            const newAd = page.locator('//*[@id="app"]/div[2]/header/div/div/div[3]/nav/ul/li[1]/a');
+            await newAd.click();
 
-            let buttonContinue = page.locator('//*[@id="submit-username"]');
+            const [left, middle, right] = config.tests[0].matricula.split('-');
+
+            const matriculaE = page.locator('//*[@id="plateLeft"]');
+            const matriculaM = page.locator('//*[@id="plateMiddle"]');
+            const matriculaR = page.locator('//*[@id="plateRight"]')
+
+            await matriculaE.fill(left);
+            await matriculaM.fill(middle);
+            await matriculaR.fill(right);
+
+            let buttonContinue = page.locator('//*[@id="btnVerify"]');
             await buttonContinue.click();
 
-            // CAPTCHA - necessário efetuar manualmente
-            if (await page.locator('//*[@id="/html/body/div[1]/div[1]/div/div/div/form/div"]').isVisible()) {
-                await page.pause();
-            }
+            const vin = page.locator('//*[@id="VIN"]');
+            const brand = page.locator('//*[@id="IdBrand"]');
+            const model = page.locator('//*[@id="IdModel"]');
+            const version = page.locator('//*[@id="IdVersion"]');
+            const bodyType = page.locator('//*[@id="IdBodyType"]');
+            const fuel = page.locator('//*[@id="IdFuel"]');
+            const transmission = page.locator('//*[@id="IdTransmission"]');
+            const hp = page.locator('//*[@id="HP"]');
+            const cc = page.locator('//*[@id="CC"]');
+            const doors = page.locator('//*[@id="Doors"]');
+            const seats = page.locator('//*[@id="Seats"]');
+            const color = page.locator('//*[@id="IdColor"]');
+            const productionYear = page.locator('//*[@id="ProductionYear"]');
+            const oriNational = page.locator('//*[@id="nacional"]');
+            buttonContinue = page.locator('//*[@id="vehicleInfo"]/div[3]/div/a[2]');
 
-            const gender = page.locator('//*[@id="registerGender"]/div[2]');
-            const acceptTerms = page.locator('//*[@id="terms"]');
+            await vin.fill(config.tests[0].vin);
+            let brandValue = config.tests[0].brand;
+            await brand.selectOption({ value: brandValue });
+            let modelValue = config.tests[0].model;
+            await model.selectOption({ value: modelValue });
+            let versionValue = config.tests[0].version;
+            await version.selectOption({ value: versionValue });
+            let bodyValue = config.tests[0].bodyType;
+            await bodyType.selectOption({ value: bodyValue });
+            let fuelValue = config.tests[0].fuel;
+            await fuel.selectOption({ value: fuelValue });
+            let transmissionValue = config.tests[0].transmission;
+            await transmission.selectOption({ value: transmissionValue });
+            hp.fill(config.tests[0].hp);
+            cc.fill(config.tests[0].cc);
+            doors.fill(config.tests[0].doors);
+            seats.fill(config.tests[0].seats);
+            let colorValue = config.tests[0].color;
+            await color.selectOption({ value: colorValue });
+            productionYear.fill(config.tests[0].productionYear);
+            oriNational.fill(config.tests[0].productionYear);
 
-            if (!(await gender.isVisible())) {
-                console.log("Email already registered");
-                await context.close();
-            }
-
-            await page.pause(); // Pausa para colocar código de validação recebido no email
-            await gender.click();
-            await acceptTerms.click();
-
-            buttonContinue = page.locator('//*[@id="btn-token-submit"]');
             await buttonContinue.click();
 
-            const acceptCreatePassword = page.locator('//*[@id="content"]/div[3]/div[2]/a');
-            await acceptCreatePassword.click();
+            let menuOption = page.locator('//*[@id="myTab"]/li[2]/a');
+            await menuOption.click()
 
-            const password = page.locator('//*[@id="current-password"]');
-            const confirmPassword = page.locator('//*[@id="new-password"]');
+            let additionalFeatures = page.locator('//*[@id="myTabContent"]/div[2]/ul[1]/li[1]/label/span');
+            await additionalFeatures.click();
 
-            const passwordRegister = generatePassword();
-            await password.fill(passwordRegister);
-            await confirmPassword.fill(passwordRegister + '1'); // Botão não atualiza automaticamente, necessário apagar 1 caracter e dar tab
+            additionalFeatures = page.locator('//*[@id="myTabContent"]/div[2]/ul[1]/li[2]/label/span');
+            await additionalFeatures.click();
 
-            await confirmPassword.press('End')
-            await confirmPassword.press('Backspace');
-            await confirmPassword.press('Tab');
-
-            console.log(passwordRegister);
-
-            await page.pause(); // Pausa para colocar código de validação recebido no email
-
-            buttonContinue = page.locator('//*[@id="btn-add-password"]');
+            buttonContinue = page.locator('//*[@id="outros"]/div/div[2]/a[2]');
             await buttonContinue.click();
 
 
-            const acceptConditions = page.locator('//*[@id="user"]/section/div/div/section/div/form/fieldset[2]/div[1]/ul/li/label');
-            await acceptConditions.click();
+            const kms = page.locator('//*[@id="Kms"]');
+            const price = page.locator('//*[@id="Price"]');
+            const description = page.locator('//*[@id="Description"]');
 
-            const saveConditions = page.locator('//*[@id="user"]/section/div/div/section/div/form/div/div/button[2]');
-            await saveConditions.click();
+            kms.fill(config.tests[0].kms);
+            price.fill(config.tests[0].price);
+            description.fill(config.tests[0].description);
 
-            await page.waitForTimeout(2000);
+            buttonContinue = page.locator('//*[@id="outros"]/div/div[2]/a[2]');
+            await buttonContinue.click();
 
-            const isLoggedIn = page.locator('//*[@id="navbarCollapse"]/ul/li[6]/div/span');
+            buttonContinue = page.locator('//*[@id="fotos"]/div[2]/a[2]');
+            await buttonContinue.click();
 
-            const emailText = await isLoggedIn.textContent();
-            expect(emailText).toContain('Olá');
+            buttonContinue = page.locator('//*[@id="dados"]/div/div[3]/a[2]');
+            await buttonContinue.click();
 
+            buttonContinue = page.locator('//*[@id="confirmChangesModal"]/div/div/div[3]/button[2]');
+            await buttonContinue.click();
+
+            const vehicleName = page.locator('//*[@id="vehicles-catalog"]/section/div[3]/div[1]/h2[1]');
+            kms = page.locator('//*[@id="vehicles-catalog"]/section/div[3]/div[1]/ul/li[1]/span[1]');
+            productionYear = page.locator('//*[@id="vehicles-catalog"]/section/div[3]/div[1]/ul/li[1]/span[2]');
+            const matricula = page.locator('//*[@id="vehicles-catalog"]/section/div[3]/div[1]/ul/li[1]/span[3]');
+
+            expect(vehicleName.textContent()).toBe('Audi A4');
+            expect(kms.textContent()).toBe('20.000 Km');
+            expect(productionYear.textContent()).toBe(2020);
+            expect(matricula.textContent()).toBe('10-AA-15')
 
         });
-
-        test('should not register an user because passwords doesnt have the min requirements', async ({ context }) => {
-
-            await page.goto(config.global);
-
-            const buttonRegister = page.locator('//*[@id="navbarCollapse"]/ul/li[6]/button');
-            if (await buttonRegister.isVisible()) {
-                await buttonRegister.click();
-            }
-
-            const emailInput = page.locator('//*[@id="username"]');
-            await emailInput.fill(config.tests[1].email);
-
-            let buttonContinue = page.locator('//*[@id="submit-username"]');
-            await buttonContinue.click();
-
-            if (await page.locator('//*[@id="/html/body/div[1]/div[1]/div/div/div/form/div"]').isVisible()) {
-                await page.pause();
-            }
-
-            const gender = page.locator('//*[@id="registerGender"]/div[2]');
-            const acceptTerms = page.locator('//*[@id="terms"]');
-
-            if (!(await gender.isVisible())) {
-                console.log("Email already registered");
-                await context.close();
-            }
-
-            await page.pause(); // Pausa para colocar código de validação recebido no email
-            await gender.click();
-            await acceptTerms.click();
-
-            buttonContinue = page.locator('//*[@id="btn-token-submit"]');
-            await page.waitForTimeout(3000);
-            await buttonContinue.click();
-
-            const acceptCreatePassword = page.locator('//*[@id="content"]/div[3]/div[2]/a');
-            await acceptCreatePassword.click();
-
-            const password = page.locator('//*[@id="current-password"]');
-            const confirmPassword = page.locator('//*[@id="new-password"]');
-            const logo = page.locator('//*[@id="content"]/header/div');
-            const rulesList = page.locator('ul.align-left.rules li');
-
-            await password.fill('1234567890');
-            await confirmPassword.fill('1234567890');
-
-            await logo.click();
-
-            const invalidRules = await rulesList.evaluateAll((elements) => {
-                return elements.filter(element => !element.classList.contains('icon-ok')).length;
-            })
-
-            buttonContinue = page.locator('//*[@id="btn-add-password"]');
-
-            expect(buttonContinue).toBeDisabled();
-            expect(invalidRules).toBeGreaterThan(0);
-
-        });
-
 
     });
 
